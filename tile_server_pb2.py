0025a09d705c619a16421e675a19ed068feeefc5
Doruk Ozturk <doruk.ozturk@kitware.com>
2019-10-25 09:36:25 -0400

    Add path parameter to the message

--- tile_server_pb2.py
+++ tile_server_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x11tile_server.proto\"*\n\x07TileXYZ\x12\t\n\x01x\x18\x01 \x01(\x05\x12\t\n\x01y\x18\x02 \x01(\x05\x12\t\n\x01z\x18\x03 \x01(\x05\"\x14\n\x04Tile\x12\x0c\n\x04tile\x18\x01 \x01(\x0c\x32*\n\nTileServer\x12\x1c\n\x07GetTile\x12\x08.TileXYZ\x1a\x05.Tile\"\x00\x62\x06proto3')
+  serialized_pb=_b('\n\x11tile_server.proto\"=\n\x07TileXYZ\x12\x11\n\ttiff_path\x18\x01 \x01(\t\x12\t\n\x01x\x18\x02 \x01(\x05\x12\t\n\x01y\x18\x03 \x01(\x05\x12\t\n\x01z\x18\x04 \x01(\x05\"\x14\n\x04Tile\x12\x0c\n\x04tile\x18\x01 \x01(\x0c\x32*\n\nTileServer\x12\x1c\n\x07GetTile\x12\x08.TileXYZ\x1a\x05.Tile\"\x00\x62\x06proto3')
 )
 
 
@@ -34,22 +34,29 @@ _TILEXYZ = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='x', full_name='TileXYZ.x', index=0,
-      number=1, type=5, cpp_type=1, label=1,
-      has_default_value=False, default_value=0,
+      name='tiff_path', full_name='TileXYZ.tiff_path', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='y', full_name='TileXYZ.y', index=1,
+      name='x', full_name='TileXYZ.x', index=1,
       number=2, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='z', full_name='TileXYZ.z', index=2,
+      name='y', full_name='TileXYZ.y', index=2,
       number=3, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='z', full_name='TileXYZ.z', index=3,
+      number=4, type=5, cpp_type=1, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -67,7 +74,7 @@ _TILEXYZ = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=21,
-  serialized_end=63,
+  serialized_end=82,
 )
 
 
@@ -97,8 +104,8 @@ _TILE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=65,
-  serialized_end=85,
+  serialized_start=84,
+  serialized_end=104,
 )
 
 DESCRIPTOR.message_types_by_name['TileXYZ'] = _TILEXYZ
@@ -127,8 +134,8 @@ _TILESERVER = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=None,
-  serialized_start=87,
-  serialized_end=129,
+  serialized_start=106,
+  serialized_end=148,
   methods=[
   _descriptor.MethodDescriptor(
     name='GetTile',
